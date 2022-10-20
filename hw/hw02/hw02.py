da2046a2270fbc3f7f691a459317b304b53147ce
Yanshi Xu <xuyanshi1999@gmail.com>
2022-10-20 19:45:28 +0800

    add files

---
+++ hw/hw02/hw02.py
@@ -0,0 +1,95 @@
+from operator import add, mul
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+HW_SOURCE_FILE = __file__
+
+
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+
+    n: a positive integer
+    term:  a function that takes one argument to produce the term
+
+    >>> product(3, identity)  # 1 * 2 * 3
+    6
+    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)    # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
+    24
+    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
+    162
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def accumulate(merger, start, n, term):
+    """Return the result of merging the first n terms in a sequence and start.
+    The terms to be merged are term(1), term(2), ..., term(n). merger is a
+    two-argument commutative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    >>> # ((2 * 1^2 * 2) * 2^2 * 2) * 3^2 * 2
+    >>> accumulate(lambda x, y: 2 * x * y, 2, 3, square)
+    576
+    >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
+    16
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def summation_using_accumulate(n, term):
+    """Returns the sum: term(0) + ... + term(n), using accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    >>> # You aren't expected to understand the code of this test.
+    >>> # Check that the bodies of the functions are just return statements.
+    >>> # If this errors, make sure you have removed the "***YOUR CODE HERE***".
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def product_using_accumulate(n, term):
+    """Returns the product: term(1) * ... * term(n), using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    >>> # You aren't expected to understand the code of this test.
+    >>> # Check that the bodies of the functions are just return statements.
+    >>> # If this errors, make sure you have removed the "***YOUR CODE HERE***".
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
+    """
+    "*** YOUR CODE HERE ***"
---
+++ labs/lab03/lab03.py
@@ -0,0 +1,210 @@
+from operator import add, mul
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+def ordered_digits(x):
+    """Return True if the (base 10) digits of X>0 are in non-decreasing
+    order, and False otherwise.
+
+    >>> ordered_digits(5)
+    True
+    >>> ordered_digits(11)
+    True
+    >>> ordered_digits(127)
+    True
+    >>> ordered_digits(1357)
+    True
+    >>> ordered_digits(21)
+    False
+    >>> result = ordered_digits(1375) # Return, don't print
+    >>> result
+    False
+
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def get_k_run_starter(n, k):
+    """Returns the 0th digit of the kth increasing run within n.
+    >>> get_k_run_starter(123444345, 0) # example from description
+    3
+    >>> get_k_run_starter(123444345, 1)
+    4
+    >>> get_k_run_starter(123444345, 2)
+    4
+    >>> get_k_run_starter(123444345, 3)
+    1
+    >>> get_k_run_starter(123412341234, 1)
+    1
+    >>> get_k_run_starter(1234234534564567, 0)
+    4
+    >>> get_k_run_starter(1234234534564567, 1)
+    3
+    >>> get_k_run_starter(1234234534564567, 2)
+    2
+    """
+    i = 0
+    final = None
+    while ____________________________:
+        while ____________________________:
+            ____________________________
+        final = ____________________________
+        i = ____________________________
+        n = ____________________________
+    return final
+
+
+def make_repeater(func, n):
+    """Return the function that computes the nth application of func.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> make_repeater(square, 0)(5) # Yes, it makes sense to apply the function zero times!
+    5
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def composer(func1, func2):
+    """Return a function f, such that f(x) = func1(func2(x))."""
+    def f(x):
+        return func1(func2(x))
+    return f
+
+
+def apply_twice(func):
+    """ Return a function that applies func twice.
+
+    func -- a function that takes one argument
+
+    >>> apply_twice(square)(2)
+    16
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def div_by_primes_under(n):
+    """
+    >>> div_by_primes_under(10)(11)
+    False
+    >>> div_by_primes_under(10)(121)
+    False
+    >>> div_by_primes_under(10)(12)
+    True
+    >>> div_by_primes_under(5)(1)
+    False
+    """
+    checker = lambda x: False
+    i = ____________________________
+    while ____________________________:
+        if not checker(i):
+            checker = ____________________________
+        i = ____________________________
+    return ____________________________
+
+
+def div_by_primes_under_no_lambda(n):
+    """
+    >>> div_by_primes_under_no_lambda(10)(11)
+    False
+    >>> div_by_primes_under_no_lambda(10)(121)
+    False
+    >>> div_by_primes_under_no_lambda(10)(12)
+    True
+    >>> div_by_primes_under_no_lambda(5)(1)
+    False
+    """
+    def checker(x):
+        return False
+    i = ____________________________
+    while ____________________________:
+        if not checker(i):
+            def outer(____________________________):
+                def inner(____________________________):
+                    return ____________________________
+                return ____________________________
+            checker = ____________________________
+        i = ____________________________
+    return ____________________________
+
+
+def zero(f):
+    return lambda x: x
+
+
+def successor(n):
+    return lambda f: lambda x: f(n(f)(x))
+
+
+def one(f):
+    """Church numeral 1: same as successor(zero)"""
+    "*** YOUR CODE HERE ***"
+
+
+def two(f):
+    """Church numeral 2: same as successor(successor(zero))"""
+    "*** YOUR CODE HERE ***"
+
+
+three = successor(two)
+
+
+def church_to_int(n):
+    """Convert the Church numeral n to a Python integer.
+
+    >>> church_to_int(zero)
+    0
+    >>> church_to_int(one)
+    1
+    >>> church_to_int(two)
+    2
+    >>> church_to_int(three)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def add_church(m, n):
+    """Return the Church numeral for m + n, for Church numerals m and n.
+
+    >>> church_to_int(add_church(two, three))
+    5
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def mul_church(m, n):
+    """Return the Church numeral for m * n, for Church numerals m and n.
+
+    >>> four = successor(three)
+    >>> church_to_int(mul_church(two, three))
+    6
+    >>> church_to_int(mul_church(three, four))
+    12
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def pow_church(m, n):
+    """Return the Church numeral m ** n, for Church numerals m and n.
+
+    >>> church_to_int(pow_church(two, three))
+    8
+    >>> church_to_int(pow_church(three, two))
+    9
+    """
+    "*** YOUR CODE HERE ***"
