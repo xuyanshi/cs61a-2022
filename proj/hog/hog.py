6ac80d3ed71383b5c6527c0d55145796c3252b53
Yanshi Xu <xuyanshi1999@gmail.com>
2022-10-22 14:30:44 +0800

    proj1 problem12
    Point breakdown
        Question 0: 0.0/0
        Question 1: 2.0/2
        Question 2: 2.0/2
        Question 3: 2.0/2
        Question 4: 1.0/1
        Question 5: 5.0/5
        Question 6: 2.0/2
        Question 7: 2.0/2
        Question 8: 2.0/2
        Question 9: 2.0/2
        Question 10: 2.0/2
        Question 11: 2.0/2
        Question 12: 0.0/0
    
    Score:
        Total: 24.0

--- proj/hog/hog.py
+++ proj/hog/hog.py
@@ -343,8 +343,8 @@ def final_strategy(score, opponent_score):
     *** YOUR DESCRIPTION HERE ***
     """
     # BEGIN PROBLEM 12
-    
-    return 6  # Remove this line once implemented.
+    return 0
+    # return 6  # Remove this line once implemented.
     # END PROBLEM 12
 
 
