2d362960fd0fa448f89bfee0d966348c295c2854
Yanshi Xu <xuyanshi1999@gmail.com>
2022-10-22 19:55:52 +0800

    hw02 finished

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -32,6 +32,10 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
+    prod=1
+    for i in range(n):
+        prod *= term(i+1)
+    return prod
 
 
 def accumulate(merger, start, n, term):
@@ -59,6 +63,9 @@ def accumulate(merger, start, n, term):
     16
     """
     "*** YOUR CODE HERE ***"
+    for i in range(n):
+        start = merger(start,term(i+1))
+    return start
 
 
 def summation_using_accumulate(n, term):
@@ -75,8 +82,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
-
+    return accumulate(add,0,n,term)
 
 def product_using_accumulate(n, term):
     """Returns the product: term(1) * ... * term(n), using accumulate.
@@ -92,4 +98,4 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(mul,1,n,term)
