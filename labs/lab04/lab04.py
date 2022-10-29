db0b8caecbf995122b70ab05051d762386493ee5
Yanshi Xu <xuyanshi1999@gmail.com>
2022-10-30 02:26:15 +0800

    lab4 q1 q2

--- labs/lab04/lab04.py
+++ labs/lab04/lab04.py
@@ -19,8 +19,10 @@ def summation(n, term):
     True
     """
     assert n >= 1
-    "*** YOUR CODE HERE ***"
-
+    if n==1:
+        return term(n)
+    else:
+        return term(n)+summation(n-1,term)
 
 def pascal(row, column):
     """Returns the value of the item in Pascal's Triangle
--- labs/lab04/tests/squared-virfib-wwpd.py
+++ labs/lab04/tests/squared-virfib-wwpd.py
@@ -12,46 +12,38 @@ test = {
           ...         return n
           ...     return (virfib_sq(n - 1) + virfib_sq(n - 2)) ** 2
           >>> r0 = virfib_sq(0)
-          c4baa133341c5988be93c04ac3d055bb
-          # locked
+          0
           >>> r1 = virfib_sq(1)
-          91b35c416e4c4ef4138ebfcce69874af
-          # locked
+          1
           >>> r2 = virfib_sq(2)
-          61b793952531daad90d65377b695da99
-          91b35c416e4c4ef4138ebfcce69874af
-          c4baa133341c5988be93c04ac3d055bb
-          # locked
+          2
+          1
+          0
           >>> r3 = virfib_sq(3)
-          62cb7be5b3f27b8761401e9f99897a30
-          61b793952531daad90d65377b695da99
-          91b35c416e4c4ef4138ebfcce69874af
-          c4baa133341c5988be93c04ac3d055bb
-          91b35c416e4c4ef4138ebfcce69874af
-          # locked
+          3
+          2
+          1
+          0
+          1
           >>> r3
-          e6efc1fcfbebed28c5068a807b6cce64
-          # locked
+          4
           >>> (r1 + r2) ** 2
-          e6efc1fcfbebed28c5068a807b6cce64
-          # locked
+          4
           >>> r4 = virfib_sq(4)
-          e6efc1fcfbebed28c5068a807b6cce64
-          62cb7be5b3f27b8761401e9f99897a30
-          61b793952531daad90d65377b695da99
-          91b35c416e4c4ef4138ebfcce69874af
-          c4baa133341c5988be93c04ac3d055bb
-          91b35c416e4c4ef4138ebfcce69874af
-          61b793952531daad90d65377b695da99
-          91b35c416e4c4ef4138ebfcce69874af
-          c4baa133341c5988be93c04ac3d055bb
-          # locked
+          4
+          3
+          2
+          1
+          0
+          1
+          2
+          1
+          0
           >>> r4
-          6958307009d94c1d298ae9f450f606ff
-          # locked
+          25
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
