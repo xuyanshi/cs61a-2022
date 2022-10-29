f4b18fa8fc10163ddd2158e7b00f60999b28966a
Yanshi Xu <xuyanshi1999@gmail.com>
2022-10-30 03:34:11 +0800

    lab4

--- labs/lab04/lab04.py
+++ labs/lab04/lab04.py
@@ -138,3 +138,4 @@ def riffle(deck): # so difficult, copied from solution
     [0, 10, 1, 11, 2, 12, 3, 13, 4, 14, 5, 15, 6, 16, 7, 17, 8, 18, 9, 19]
     """
     return [deck[(i%2)*len(deck)//2+i//2] for i in range(len(deck))]
+
