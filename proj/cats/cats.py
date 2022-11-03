e5f9b3c25ac42b8dcf8b6d837fa75d1d1af04475
Yanshi Xu <xuyanshi1999@gmail.com>
2022-11-03 18:42:07 +0800

    proj2 q10

--- proj/cats/cats.py
+++ proj/cats/cats.py
@@ -378,6 +378,23 @@ def fastest_words(match):
     word_indices = range(len(get_all_words(match)))  # contains an *index* for each word
     # BEGIN PROBLEM 10
     "*** YOUR CODE HERE ***"
+    # player_cnt = len(get_all_times(match))
+    # word_cnt = len(get_all_words(match))
+    # Do not need these values.
+    fastest = [None] * len(get_all_times(match))
+    for j in player_indices:
+        fastest[j] = []
+
+    for i in word_indices:
+        word = get_word(match, i)
+        min_j, min_time = 0, time(match, 0, i)
+        for j in player_indices:
+            player = time(match, j, i)
+            if player < min_time:
+                min_time = player
+                min_j = j
+        fastest[min_j].append(word)
+    return fastest
     # END PROBLEM 10
 
 
