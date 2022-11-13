a1c11a9395c37137819ab2b28a4999e20838a749
Yanshi XU <xuyanshi1999@gmail.com>
2022-11-13 23:12:13 +0800

    lab8 q1

--- lab/lab08/tests/link.py
+++ lab/lab08/tests/link.py
@@ -9,20 +9,16 @@ test = {
           >>> from lab08 import *
           >>> link = Link(1000)
           >>> link.first
-          6812c36ce75fc0b1c442b1d56b8c98f1
-          # locked
+          1000
           >>> link.rest is Link.empty
-          0528ddea472f19174e0c4eb75b4de3de
-          # locked
+          True
           >>> link = Link(1000, 2000)
-          8a2fd4e4c3c6dcce2dc631af62ee217a
-          # locked
+          Error
           >>> link = Link(1000, Link())
-          8a2fd4e4c3c6dcce2dc631af62ee217a
-          # locked
+          Error
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
@@ -30,41 +26,32 @@ test = {
           >>> from lab08 import *
           >>> link = Link(1, Link(2, Link(3)))
           >>> link.first
-          802285b020b27240a3824a7e42f8cc8c
-          # locked
+          1
           >>> link.rest.first
-          9338923f09aac77121029c604f7ce4f3
-          # locked
+          2
           >>> link.rest.rest.rest is Link.empty
-          0528ddea472f19174e0c4eb75b4de3de
-          # locked
+          True
           >>> link.first = 9001
           >>> link.first
-          2f870cb7220a96bf2531180ebc182058
-          # locked
+          9001
           >>> link.rest = link.rest.rest
           >>> link.rest.first
-          a6a221ff20ce085ab4bedaca5044f971
-          # locked
+          3
           >>> link = Link(1)
           >>> link.rest = link
           >>> link.rest.rest is Link.empty
-          9b87ee740b72a262114634ab6b9e401e
-          # locked
+          False
           >>> link.rest.rest.rest.rest.first
-          802285b020b27240a3824a7e42f8cc8c
-          # locked
+          1
           >>> link = Link(2, Link(3, Link(4)))
           >>> link2 = Link(1, link)
           >>> link2.first
-          802285b020b27240a3824a7e42f8cc8c
-          # locked
+          1
           >>> link2.rest.first
-          9338923f09aac77121029c604f7ce4f3
-          # locked
+          2
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
@@ -72,14 +59,12 @@ test = {
           >>> from lab08 import *
           >>> link = Link(5, Link(6, Link(7)))
           >>> link                  # Look at the __repr__ method of Link
-          c4be84510df152813dfac1955471823d
-          # locked
+          Link(5, Link(6, Link(7)))
           >>> print(link)          # Look at the __str__ method of Link
-          9e4aa8611562bf27deef2ea24cf05283
-          # locked
+          <5 6 7>
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
