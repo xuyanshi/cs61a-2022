a0ff8e31538cdd987cbc99abbb95765295aa64a2
Yanshi XU <xuyanshi1999@gmail.com>
2022-11-16 22:25:05 +0800

    lab10 q3

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,4 +1,10 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond
+    ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    (else 1)
+    )
+  )
 
 (define (make-adder num) 'YOUR-CODE-HERE)
 
