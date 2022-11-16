e694bee5d912b131174827a92282e75da9df03c5
Yanshi XU <xuyanshi1999@gmail.com>
2022-11-16 21:56:55 +0800

    hw7 q3

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -16,4 +16,11 @@
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (cond
+    ((= exp 0) 1)
+    ((= base 1) 1)
+    ((even? exp) (square (pow base (/ exp 2))) )
+    (else (* base (pow base (- exp 1))))
+    )
+  )
