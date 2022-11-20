9909dd846d20724d208f6812a1f9e2a95adcb4ad
Yanshi XU <xuyanshi1999@gmail.com>
2022-11-20 13:30:25 +0800

    hw8 q1

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,5 +1,9 @@
-(define (my-filter pred s)
-  
+(define (my-filter pred lst)
+  (cond
+          ((null? lst) lst)
+          ((pred (car lst))(cons (car lst) (my-filter pred (cdr lst))))
+          (else (my-filter pred (cdr lst)))
+          )
   )
 
 (define (interleave lst1 lst2) 'YOUR-CODE-HERE)
