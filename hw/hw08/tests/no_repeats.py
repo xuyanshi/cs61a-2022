bef2f08b6622097998eb85a6c7e8cc90281e3664
Yanshi XU <xuyanshi1999@gmail.com>
2022-11-20 14:08:50 +0800

    hw8 q4

--- hw/hw08/tests/no_repeats.py
+++ hw/hw08/tests/no_repeats.py
@@ -7,51 +7,46 @@ test = {
         {
           'code': r"""
           scm> (no-repeats '(5 4 3 2 1))
-          2cd8b7f491d49051fe3a86c912bff438
-          # locked
+          (5 4 3 2 1)
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (no-repeats '(5 4 3 2 1 1))
-          2cd8b7f491d49051fe3a86c912bff438
-          # locked
+          (5 4 3 2 1)
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (no-repeats '(5 5 4 3 2 1))
-          2cd8b7f491d49051fe3a86c912bff438
-          # locked
+          (5 4 3 2 1)
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (no-repeats '(12))
-          0987f17f42b47ec069f680a50c44ae4e
-          # locked
+          (12)
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (no-repeats '(1 1 1 1 1 1))
-          4fbb2195709ce6677a192b31dd920a41
-          # locked
+          (1)
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
@@ -67,27 +62,23 @@ test = {
         {
           'code': r"""
           scm> (no-repeats (list 5 4 2))
-          6f2d21acf7bbeceb3feb85fda8239146
-          # locked
+          (5 4 2)
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (no-repeats (list 5 4 5 4 2 2))
-          6f2d21acf7bbeceb3feb85fda8239146
-          # locked
+          (5 4 2)
           scm> (no-repeats (list 5 5 5 5 5))
-          ff38b3852cb7a2250c1ac7c480743135
-          # locked
+          (5)
           scm> (no-repeats ())
-          7e44d32911eb855f7a970358ab156a57
-          # locked
+          ()
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
